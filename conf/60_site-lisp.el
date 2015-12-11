e82c493a8d1d138527bf3789df8afcd35e3e50c2
conao <conao3@gmail.com>
2015-12-12 08:52:47 +0900

    setting swap

--- conf/60_site-lisp.el
+++ conf/60_site-lisp.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015 Naoya Yamashita
 ;; Author: Naoya Yamashita
 ;; Created:      <2015/12/10 05:38:37>
-;; Last-Updated: <2015/12/12 08:48:30>
+;; Last-Updated: <2015/12/12 08:52:31>
 ;; Keywords: 
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -74,3 +74,44 @@
   :defer t
   :config (progn (smartparens-global-mode)
                  (sp-pair "$" "$")))
+
+(use-package swap
+  ;; swap-rectangle
+
+  ;; とある二つの rectangle をいれかえます.
+  ;; たとえば
+  ;; A I X
+  ;; B J Y
+  ;; C K Z
+  ;; という状態から,
+  ;; X I A
+  ;; Y J B
+  ;; Z K C
+  ;; みたいに変更したい場合, レジスタを使ったり kill-rectangle を使ったりする方法だと ちょっと余計な手間がかかりますよね.
+  ;; この程度のことを一発でできる elisp くらい既にありそうなもんですが, 私の調べた限 りでは見付からなかったのでした.
+  ;; 結構こういう処理をしたいことが多かったので, 作ってみました.
+  ;; 使い方は,
+  ;; M-x swap-rectangle
+  ;; いれかえたい片方の rectangle の開始位置で C-SPC
+  ;; いれかえたい片方の rectangle の終了位置で Enter(ここまでがハイライトされます)
+  ;; いれかえたいもう一方の rectangle の開始位置で C-SPC
+  ;; いれかえたいもう一方の rectangle の終了位置で Enter
+  ;; とするだけです.
+  ;; やめるときは C-g で.
+  ;; たぶんやってみればすぐわかるかと.
+
+  ;; swap-region
+
+  ;; 二つの領域をいれかえます.
+  ;; まあ emacs なんだから yank-pop 使えばちょっとの手間でできますけど, せっかくだか ら region をいれかえるのも作ってみたわけです.
+  ;; 使い方は,
+  ;; M-x swap-region
+  ;; いれかえたい片方の region の開始位置で C-SPC
+  ;; いれかえたい片方の region の終了位置で Enter
+  ;; いれかえたいもう一方の region の開始位置で C-SPC
+  ;; いれかえたいもう一方の region の終了位置で Enter
+  ;; とするだけです.
+  ;; やめるときは C-g で.
+  ;; たぶんやってみればすぐわかるかと.
+
+  :commands swap-region swap-rectangle)
