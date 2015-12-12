3e3efffc708dce3991062cd3f75ddaace7e93c86
conao <conao3@gmail.com>
2015-12-12 09:07:45 +0900

    setting auto-install, tab-jump-out, w3m

--- conf/30_utility.el
+++ conf/30_utility.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015 Naoya Yamashita
 ;; Author: Naoya Yamashita
 ;; Created:      <2015/12/10 05:38:03>
-;; Last-Updated: <2015/12/12 08:47:05>
+;; Last-Updated: <2015/12/12 09:05:39>
 ;; Keywords: 
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -79,4 +79,25 @@
 (use-package magit
   :ensure t
   :defer  t)
+
+(use-package auto-install
+  :ensure t
+  :defer  t
+  :config (setq auto-install-directory "~/.emacs.d/site-lisp"))
+
+(use-package tab-jump-out
+  :ensure t
+  :defer  t
+  :config (setq yas-fallback-behavior '(apply tab-jump-out 1)))
+
+(use-package w3m)
+
+
+
+
+
+
+
+
+
 
