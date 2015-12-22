211fe05689a5fa31f4edd62fb32111cfd5de14b0
conao <conao3@gmail.com>
2015-12-22 09:03:24 +0900

    fix typo

--- conf/70_conao-lisp.el
+++ conf/70_conao-lisp.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015 Naoya Yamashita
 ;; Author: Naoya Yamashita
 ;; Created:      <2015/12/10 05:38:52>
-;; Last-Updated: <2015/12/16 16:22:40>
+;; Last-Updated: <2015/12/16 17:09:17>
 ;; Keywords: 
 
 ;; This program is free software; you can redistribute it and/or modify
