fba5f50b8db7ec2a6b265d2fbb2b4f61cf00762c
conao <conao3@gmail.com>
2015-12-24 14:33:15 +0900

    change magit bind

--- conf/30_utility.el
+++ conf/30_utility.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015 Naoya Yamashita
 ;; Author: Naoya Yamashita
 ;; Created:      <2015/12/10 05:38:03>
-;; Last-Updated: <2015/12/23 22:06:28>
+;; Last-Updated: <2015/12/24 14:33:05>
 ;; Keywords: 
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -89,7 +89,7 @@
 (use-package magit
   :ensure t
   :defer  t
-  :bind  (("C-v" . magit-status)))
+  :bind  (("C-x v" . magit-status)))
 
 (use-package auto-install
   :ensure t
