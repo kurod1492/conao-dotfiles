4fe16017cfc15b343777cb6b1262a4f03e26af23
conao <conao3@gmail.com>
2017-06-07 23:23:16 +0900

    el-get

--- .emacs.d/conf/00_use-package.el
+++ .emacs.d/conf/00_use-package.el
@@ -1,3 +1,28 @@
+;;; 00_use-package.el ---                                 -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017 Naoya Yamashita
+
+;; Author: Naoya Yamashita
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
 
 (when (not (package-installed-p 'use-package))
   (package-refresh-contents)
@@ -20,11 +45,14 @@
 (defsubst hook-into-modes (func &rest modes)
   (dolist (mode-hook modes) (add-hook mode-hook func)))
 
-(defmacro el-get-conao (package)
-  (unless (require package nil noerror)
-    (el-get-bundle (concat "conao/" package))))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
 (eval-when-compile
   (require 'use-package)
-(require 'diminish)              
+(require 'diminish)
 (require 'bind-key))
