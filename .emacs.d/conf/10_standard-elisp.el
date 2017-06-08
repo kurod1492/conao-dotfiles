b3b7216093f321159d0bd2e7d0606b5b78b63602
Naoya Yamashita <conao3@gmail.com>
2017-06-08 14:44:16 +0900

    add file comment, delete unnessesaey setq

--- .emacs.d/conf/10_standard-elisp.el
+++ .emacs.d/conf/10_standard-elisp.el
@@ -1,4 +1,26 @@
-;;; 10_standard-elisp.el
+;;; 10_standard-elisp.el ---                                -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao@Naoya-MacBook-Air.local>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
 
 ;;; Code:
 
@@ -35,7 +57,6 @@
   ;; %s   -- マシン名
   ;; %u   -- ログインしたユーザ名
   ;; %U   -- ログインしたユーザのフルネーム
-  ;; :defer t
   :config
   (add-hook 'before-save-hook 'time-stamp)
   (setq time-stamp-active t
@@ -51,8 +72,7 @@
 (use-package linum
   :config
   (global-linum-mode t)
-  (setq linum-delay nil
-        linum-format "%5d")
+  (setq linum-format "%5d")
   (set-face-attribute 'linum nil :height 120))
 
 (use-package newcomment
