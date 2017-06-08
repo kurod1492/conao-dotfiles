3f74d6ee447383c365792a9216e5e0a28fda9ac3
Naoya Yamashita <conao3@gmail.com>
2017-06-08 16:36:55 +0900

    delete while spaces

--- .emacs.d/conf/30_utility.el
+++ .emacs.d/conf/30_utility.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2017  Naoya Yamashita
 
 ;; Author: Naoya Yamashita <conao@Naoya-MacBook-Air.local>
-;; Keywords: 
+;; Keywords:
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
