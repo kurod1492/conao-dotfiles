28a8ca26a3cf70a67615ea4d407d3aef9fc1bd5b
Conao <conao3@gmail.com>
2017-06-13 01:52:14 +0900

    pdf-tools

--- .emacs.d/conf/30_utility.el
+++ .emacs.d/conf/30_utility.el
@@ -119,6 +119,29 @@
   :config
   (use-package elscreen-persist :ensure t))
 
+(use-package pdf-tools :ensure t :disabled t
+  :config
+  ;; depend glib, poppler, ghostscript, imagemagick
+  ;; brew install glib poppler ghostscript imagemagick
+  (pdf-tools-install)
+
+  ;; linum mode off in pdf-mode
+  (defcustom linum-disabled-modes-list '(doc-view-mode pdf-view-mode)
+    "* List of modes disabled when global linum mode is on"
+    :type '(repeat (sexp :tag "Major mode"))
+    :tag " Major modes where linum is disabled: "
+    :group 'linum
+    )
+  (defcustom linum-disable-starred-buffers 't
+    "* Disable buffers that have stars in them like *Gnu Emacs*"
+    :type 'boolean
+    :group 'linum)
+  (defun linum-on ()
+    "* When linum is running globally, disable line number in modes defined in `linum-disabled-modes-list'. Changed by linum-off. Also turns off numbering in starred modes like *scratch*"
+    (unless (or (minibufferp) (member major-mode linum-disabled-modes-list)
+                (and linum-disable-starred-buffers (string-match "*" (buffer-name))))
+      (linum-mode 1)))
+  )
 
 ;; el-get packages
 (use-package other-window-or-split
