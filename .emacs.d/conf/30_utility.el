645344bd4d78b63bc46dfe8c85f7a896b7ef3ede
Naoya Yamashita <conao3@gmail.com>
2017-06-14 00:25:52 +0900

    setting dired subtree

--- .emacs.d/conf/30_utility.el
+++ .emacs.d/conf/30_utility.el
@@ -176,6 +176,46 @@ Also turns off numbering in starred modes like *scratch*"
   :init
   (add-hook 'dired-mode-hook 'dired-filter-mode))
 
+(use-package dired-subtree :ensure t :defer t
+  :commands dired-subtree-insert
+  :init
+  (use-package dired-details :ensure t)
+  (bind-keys :map dired-mode-map
+             ("i" . dired-subtree-insert))
+  :config
+  ;; org-modeのようにTABで折り畳む
+  (define-key dired-mode-map (kbd "<tab>") 'dired-subtree-remove)
+  ;; C-x n nでsubtreeにナローイング
+  (define-key dired-mode-map (kbd "C-x n n") 'dired-subtree-narrow)
+
+  ;; ファイル名以外の情報を(と)で隠したり表示したり
+  (dired-details-install)
+  (setq dired-details-hidden-string "")
+  (setq dired-details-hide-link-targets nil)
+  (setq dired-details-initially-hide nil)
+
+  ;; dired-subtreeをdired-detailsに対応させる
+  (defun dired-subtree-after-insert-hook--dired-details ()
+    (dired-details-delete-overlays)
+    (dired-details-activate))
+  (add-hook 'dired-subtree-after-insert-hook
+            'dired-subtree-after-insert-hook--dired-details)
+
+  ;; find-dired対応
+  (defadvice find-dired-sentinel (after dired-details (proc state) activate)
+    (ignore-errors
+      (with-current-buffer (process-buffer proc)
+        (dired-details-activate))))
+  ;; (progn (ad-disable-advice 'find-dired-sentinel 'after 'dired-details) (ad-update 'find-dired-sentinel))
+
+  ;; [2014-12-30 Tue]^をdired-subtreeに対応させる
+  (defun dired-subtree-up-dwim (&optional arg)
+    "subtreeの親ディレクトリに移動。そうでなければ親ディレクトリを開く(^の挙動)。"
+    (interactive "p")
+    (or (dired-subtree-up arg)
+        (dired-up-directory)))
+  (define-key dired-mode-map (kbd "^") 'dired-subtree-up-dwim))
+
 (use-package mode-compile  :ensure t :defer t
   :bind* (("C-c c" . mode-compile))
   :config
