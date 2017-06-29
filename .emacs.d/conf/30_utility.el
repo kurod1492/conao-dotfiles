2bb73293f431b6186887d9f50c29daa4a7fda513
Conao <conao3@gmail.com>
2017-06-29 12:59:05 +0900

    refactoring pdf-mode

--- .emacs.d/conf/30_utility.el
+++ .emacs.d/conf/30_utility.el
@@ -120,26 +120,26 @@
   ;; $ brew install glib poppler ghostscript imagemagick
   (pdf-tools-install t)
 
-  (add-to-list 'auto-mode-alist (cons "\\.pdf$" 'pdf-view-mode))
-  ;; linum mode off in pdf-mode
-  (defcustom linum-disabled-modes-list '(doc-view-mode pdf-view-mode)
-    "* List of modes disabled when global linum mode is on"
-    :type '(repeat (sexp :tag "Major mode"))
-    :tag " Major modes where linum is disabled: "
-    :group 'linum
-    )
-  (defcustom linum-disable-starred-buffers 't
-    "* Disable buffers that have stars in them like *Gnu Emacs*"
-    :type 'boolean
-    :group 'linum)
-  (defun linum-on ()
-    "* When linum is running globally,
+  (add-to-list 'auto-mode-alist '("\\.pdf$" . 'pdf-view-mode))
+  
+  (prog1 "linum mode off in pdf-mode"
+    (defcustom linum-disabled-modes-list '(doc-view-mode pdf-view-mode)
+      "* List of modes disabled when global linum mode is on"
+      :type '(repeat (sexp :tag "Major mode"))
+      :tag " Major modes where linum is disabled: "
+      :group 'linum)
+    (defcustom linum-disable-starred-buffers 't
+      "* Disable buffers that have stars in them like *Gnu Emacs*"
+      :type 'boolean
+      :group 'linum)
+    (defun linum-on ()
+      "* When linum is running globally,
 disable line number in modes defined in `linum-disabled-modes-list'.
 Changed by linum-off.
 Also turns off numbering in starred modes like *scratch*"
-    (unless (or (minibufferp) (member major-mode linum-disabled-modes-list)
-                (and linum-disable-starred-buffers (string-match "*" (buffer-name))))
-      (linum-mode 1))))
+      (unless (or (minibufferp) (member major-mode linum-disabled-modes-list)
+                  (and linum-disable-starred-buffers (string-match "*" (buffer-name))))
+        (linum-mode 1)))))
 
 (use-package auto-async-byte-compile :ensure t :defer t :disabled t
   :config
