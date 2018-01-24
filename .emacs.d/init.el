82f7555b66a833e4415b35d1d4a784ab124aed58
Naoya Yamashita <conao3@gmail.com>
2018-01-25 02:09:00 +0900

    setting init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,6 +24,40 @@
 
 ;;; Code:
 
+;; if you run like 'emacs -q -l ~/hoge/init.el'
+;; load settings in ~/hoge/
+(if load-file-name
+    (setq user-emacs-directory (file-name-directory load-file-name))
+  (setq user-emacs-directory "~/.emacs.d/"))
+
+(defmacro user-setting-directory (directory)
+  "Return user-emacs-directory/DIRECTORY to setting Emacs."
+  (concat user-emacs-directory directory))
+
+;; package
+(require 'package)
+(add-to-list 'package-archives '("gnu"       . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa"     . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("org"       . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; use-package
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; org-mode
+(use-package org :ensure t
+  :config
+  (setq org-src-preserve-indentation t)
+  
+  (defvar org-startup-dir (user-setting-directory "conf/"))
+  (defun load-org-file (file)
+    "load org file"
+    (org-babel-load-file (expand-file-name file org-startup-dir)))
+  
+  (org-babel-load-file "init.org"))
+
 (provide 'init)
 ;;; init.el ends here
-
