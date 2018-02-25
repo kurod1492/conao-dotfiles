28271801e4c6195746c1633c8e7d7ec8580bfd7c
Naoya Yamashita <conao3@gmail.com>
2018-02-26 01:57:08 +0900

    delete gnu source (default settings)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -57,7 +57,6 @@
   (add-to-load-path folder))
 
 (require 'package)
-(add-to-list 'package-archives '("gnu"       . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa"     . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("org"       . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
