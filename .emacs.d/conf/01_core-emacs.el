a7ae99eea61c3a01a0a59331ab1ad775ad6304ed
Naoya Yamashita <conao3@gmail.com>
2018-04-19 22:40:28 +0900

    create core-emacs.el

---
+++ .emacs.d/conf/01_core-emacs.el
@@ -0,0 +1,28 @@
+;;; 01_core-emacs.el ---                             -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao@245-073.vpn.hiroshima-u.ac.jp>
+;; Keywords: .emacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(provide '01_core-emacs)
+;;; 01_core-emacs.el ends here
