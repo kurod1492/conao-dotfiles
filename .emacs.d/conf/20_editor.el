6977bf5050ded9fb6ce964189ae53edd192e85b1
Naoya Yamashita <conao3@gmail.com>
2018-04-22 01:14:38 +0900

    update helm settings

--- .emacs.d/conf/20_editor.el
+++ .emacs.d/conf/20_editor.el
@@ -42,7 +42,7 @@
          :map helm-map
          ("<tab>"   . helm-execute-persistent-action)
          ("C-i"     . helm-execute-persistent-action)
-         ("C-z"   . helm-select-action))
+         ("C-z"     . helm-select-action))
   :config
   (use-package helm-config)
   
@@ -52,9 +52,9 @@
   (global-unset-key (kbd "C-x c"))
   
   (when (executable-find "curl")
-    (setq helm-google-suggest-use-curl-p t))
+    (setq helm-net-prefer-curl t))
   
-  (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+  (setq helm-split-window-inside-p            t ; open helm buffer inside current window, not occupy whole other window
         helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
         helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
         helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
