1d68d5fecb8b33fbda7ba392a94fdadb519e840a
Naoya Yamashita <conao3@gmail.com>
2018-08-04 04:53:28 +0900

    [fish] add peco bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,9 @@ set SHELL /Users/conao/local/homebrew/bin/fish
 # anyenv
 set PATH $HOME/.anyenv/bin $PATH
 anyenv init - | source
+
+# bindings
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+  bind \c] peco_select_ghq_repository
+end
