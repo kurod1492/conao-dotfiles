70f8f5ed92216cc24033d0447dbfa75e108e7d5c
conao <conao3@gmail.com>
2018-10-29 22:25:20 +0900

    add detail message

--- .emacs.d/Makefunc.mk
+++ .emacs.d/Makefunc.mk
@@ -16,14 +16,15 @@ ECHO_MAGENTA = $(call ECHO_COLOR, 5, $1)
 ECHO_CYAN    = $(call ECHO_COLOR, 6, $1)
 ECHO_WHITE   = $(call ECHO_COLOR, 7, $1)
 
-COLOR_BLACK   = 0
-COLOR_RED     = 1
-COLOR_GREEN   = 2
-COLOR_YELLOW  = 3
-COLOR_BLUE    = 4
-COLOR_MAGENTA = 5
-COLOR_CYAN    = 6
-COLOR_WHITE   = 7
+COLOR_BLACK   = tput setaf 0
+COLOR_RED     = tput setaf 1
+COLOR_GREEN   = tput setaf 2
+COLOR_YELLOW  = tput setaf 3
+COLOR_BLUE    = tput setaf 4
+COLOR_MAGENTA = tput setaf 5
+COLOR_CYAN    = tput setaf 6
+COLOR_WHITE   = tput setaf 7
+COLOR_DEFAULT = tput sgr0
 
 colortest:
 	$(call ECHO_BLACK, black)
