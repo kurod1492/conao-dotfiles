cde84ca390c47d03b16ef1a726d47ce3d2c52b1f
conao <conao3@gmail.com>
2018-10-29 16:51:55 +0900

    Makefunc.mk v1.1 add colorcode

--- .emacs.d/Makefunc.mk
+++ .emacs.d/Makefunc.mk
@@ -1,7 +1,7 @@
 #
 # Makefunk.mk
 #
-# version: v1.0
+# version: v1.1
 # last update: 2018/10/29
 #
 # echo with color
@@ -15,6 +15,15 @@ ECHO_BLUE    = $(call ECHO_COLOR, 4, $1)
 ECHO_MAGENTA = $(call ECHO_COLOR, 5, $1)
 ECHO_CYAN    = $(call ECHO_COLOR, 6, $1)
 ECHO_WHITE   = $(call ECHO_COLOR, 7, $1)
+
+COLOR_BLACK   = 0
+COLOR_RED     = 1
+COLOR_GREEN   = 2
+COLOR_YELLOW  = 3
+COLOR_BLUE    = 4
+COLOR_MAGENTA = 5
+COLOR_CYAN    = 6
+COLOR_WHITE   = 7
 
 colortest:
 	$(call ECHO_BLACK, black)
