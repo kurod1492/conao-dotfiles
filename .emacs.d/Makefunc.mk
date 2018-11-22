be707f2c5e1b34f4e5ece4dee7045ee6a70b1074
Naoya Yamashita <conao3@gmail.com>
2018-11-22 21:59:01 +0900

    update Makefunc.mk v1.3

--- .emacs.d/Makefunc.mk
+++ .emacs.d/Makefunc.mk
@@ -1,12 +1,12 @@
 #
 # Makefunk.mk
 #
-# version: v1.2
-# last update: 2018/10/29
+# version: v1.3
+# last update: 2018/11/22
 #
 # echo with color
 
-ECHO_COLOR   = tput setaf $1; echo "=== " $2 " ==="; tput sgr0
+ECHO_COLOR   = tput setaf $1; /bin/echo $3 "=== " $2 " ==="; tput sgr0
 ECHO_BLACK   = $(call ECHO_COLOR, 0, $1)
 ECHO_RED     = $(call ECHO_COLOR, 1, $1)
 ECHO_GREEN   = $(call ECHO_COLOR, 2, $1)
@@ -15,6 +15,15 @@ ECHO_BLUE    = $(call ECHO_COLOR, 4, $1)
 ECHO_MAGENTA = $(call ECHO_COLOR, 5, $1)
 ECHO_CYAN    = $(call ECHO_COLOR, 6, $1)
 ECHO_WHITE   = $(call ECHO_COLOR, 7, $1)
+
+ECHO_BLACK-   = $(call ECHO_COLOR, 0, $1, -n)
+ECHO_RED-     = $(call ECHO_COLOR, 1, $1, -n)
+ECHO_GREEN-   = $(call ECHO_COLOR, 2, $1, -n)
+ECHO_YELLOW-  = $(call ECHO_COLOR, 3, $1, -n)
+ECHO_BLUE-    = $(call ECHO_COLOR, 4, $1, -n)
+ECHO_MAGENTA- = $(call ECHO_COLOR, 5, $1, -n)
+ECHO_CYAN-    = $(call ECHO_COLOR, 6, $1, -n)
+ECHO_WHITE-   = $(call ECHO_COLOR, 7, $1, -n)
 
 COLOR_BLACK   = tput setaf 0
 COLOR_RED     = tput setaf 1
