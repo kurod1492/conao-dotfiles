4989385e0bce12330cc019980ee80615519acad5
Naoya Yamashita <conao3@gmail.com>
2018-11-22 22:13:03 +0900

    update Makefunk.mk to v1.4

--- .emacs.d/Makefunc.mk
+++ .emacs.d/Makefunc.mk
@@ -1,29 +1,34 @@
 #
 # Makefunk.mk
 #
-# version: v1.3
+# version: v1.4
 # last update: 2018/11/22
 #
 # echo with color
 
-ECHO_COLOR   = tput setaf $1; /bin/echo $3 "=== " $2 " ==="; tput sgr0
-ECHO_BLACK   = $(call ECHO_COLOR, 0, $1)
-ECHO_RED     = $(call ECHO_COLOR, 1, $1)
-ECHO_GREEN   = $(call ECHO_COLOR, 2, $1)
-ECHO_YELLOW  = $(call ECHO_COLOR, 3, $1)
-ECHO_BLUE    = $(call ECHO_COLOR, 4, $1)
-ECHO_MAGENTA = $(call ECHO_COLOR, 5, $1)
-ECHO_CYAN    = $(call ECHO_COLOR, 6, $1)
-ECHO_WHITE   = $(call ECHO_COLOR, 7, $1)
+ECHO_COLOR   = /bin/echo -n $3; \
+               tput setaf $1; \
+               /bin/echo $5 "=== " $2 " ==="; \
+               /bin/echo -n $4; \
+               tput sgr0
 
-ECHO_BLACK-   = $(call ECHO_COLOR, 0, $1, -n)
-ECHO_RED-     = $(call ECHO_COLOR, 1, $1, -n)
-ECHO_GREEN-   = $(call ECHO_COLOR, 2, $1, -n)
-ECHO_YELLOW-  = $(call ECHO_COLOR, 3, $1, -n)
-ECHO_BLUE-    = $(call ECHO_COLOR, 4, $1, -n)
-ECHO_MAGENTA- = $(call ECHO_COLOR, 5, $1, -n)
-ECHO_CYAN-    = $(call ECHO_COLOR, 6, $1, -n)
-ECHO_WHITE-   = $(call ECHO_COLOR, 7, $1, -n)
+ECHO_BLACK   = $(call ECHO_COLOR, 0, $1, $2, $3)
+ECHO_RED     = $(call ECHO_COLOR, 1, $1, $2, $3)
+ECHO_GREEN   = $(call ECHO_COLOR, 2, $1, $2, $3)
+ECHO_YELLOW  = $(call ECHO_COLOR, 3, $1, $2, $3)
+ECHO_BLUE    = $(call ECHO_COLOR, 4, $1, $2, $3)
+ECHO_MAGENTA = $(call ECHO_COLOR, 5, $1, $2, $3)
+ECHO_CYAN    = $(call ECHO_COLOR, 6, $1, $2, $3)
+ECHO_WHITE   = $(call ECHO_COLOR, 7, $1, $2, $3)
+
+ECHO_BLACK-   = $(call ECHO_COLOR, 0, $1, $2, $3, -n)
+ECHO_RED-     = $(call ECHO_COLOR, 1, $1, $2, $3, -n)
+ECHO_GREEN-   = $(call ECHO_COLOR, 2, $1, $2, $3, -n)
+ECHO_YELLOW-  = $(call ECHO_COLOR, 3, $1, $2, $3, -n)
+ECHO_BLUE-    = $(call ECHO_COLOR, 4, $1, $2, $3, -n)
+ECHO_MAGENTA- = $(call ECHO_COLOR, 5, $1, $2, $3, -n)
+ECHO_CYAN-    = $(call ECHO_COLOR, 6, $1, $2, $3, -n)
+ECHO_WHITE    = $(call ECHO_COLOR, 7, $1, $2, $3, -n)
 
 COLOR_BLACK   = tput setaf 0
 COLOR_RED     = tput setaf 1
