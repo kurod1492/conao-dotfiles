efa8d96d445eb4b0a2ee3ed74e4f903fc790688d
conao3 <conao3@gmail.com>
2019-04-05 10:09:58 +0900

    add persp-mode

--- .emacs.d/conf/30_editor.el
+++ .emacs.d/conf/30_editor.el
@@ -361,6 +361,59 @@
   ;;  (global-set-key (kbd "C-c h") helm-command-map)
   (global-unset-key (kbd "C-x c")))
 
+(leaf persp-mode
+  ;; :hook ((emacs-startup . toggle-frame-maximized))
+  :custom ((persp-keymap-prefix    . (kbd "C-c p"))
+           (persp-nil-name         . "default")
+           (persp-auto-resume-time . 1))
+  :config
+  (persp-mode 1)
+  ;; NOTE: Redefine `persp-add-new' to address.
+  ;; Issue: Unable to create/handle persp-mode
+  ;; https://github.com/Bad-ptr/persp-mode.el/issues/96
+  ;; https://github.com/Bad-ptr/persp-mode-projectile-bridge.el/issues/4
+  ;; https://emacs-china.org/t/topic/6416/7
+  ;; (defun* persp-add-new (name &optional (phash *persp-hash*))
+  ;;   "Create a new perspective with the given `NAME'. Add it to `PHASH'.
+  ;; Return the created perspective."
+  ;;   (interactive "sA name for the new perspective: ")
+  ;;   (if (and name (not (equal "" name)))
+  ;;       (destructuring-bind (e . p)
+  ;;           (persp-by-name-and-exists name phash)
+  ;;         (if e p
+  ;;           (setq p (if (equal persp-nil-name name)
+  ;;       		nil (make-persp :name name)))
+  ;;           (persp-add p phash)
+  ;;           (run-hook-with-args 'persp-created-functions p phash)
+  ;;           p))
+  ;;     (message "[persp-mode] Error: Can't create a perspective with empty name.")
+  ;;     nil))
+
+  ;; Ignore temporary buffers
+  ;; (add-hook 'persp-common-buffer-filter-functions
+  ;;           (lambda (b) (or (string-prefix-p "*" (buffer-name b))
+  ;;       	       (string-prefix-p "magit" (buffer-name b)))))
+
+  ;; Integrate IVY
+  ;; (with-eval-after-load "ivy"
+  ;;   (add-hook 'ivy-ignore-buffers
+  ;;             #'(lambda (b)
+  ;;       	  (when persp-mode
+  ;;       	    (let ((persp (get-current-persp)))
+  ;;       	      (if persp
+  ;;       		  (not (persp-contain-buffer-p b persp))
+  ;;       		nil)))))
+
+  ;;   (setq ivy-sort-functions-alist
+  ;;         (append ivy-sort-functions-alist
+  ;;       	  '((persp-kill-buffer   . nil)
+  ;;       	    (persp-remove-buffer . nil)
+  ;;       	    (persp-add-buffer    . nil)
+  ;;       	    (persp-switch        . nil)
+  ;;       	    (persp-window-switch . nil)
+  ;;       	    (persp-frame-switch  . nil)))))
+  )
+
 (leaf elscreen
   :disabled t
   :ensure t
