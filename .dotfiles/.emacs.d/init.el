a7c05f68e6145842ddabe65b07b3974f924876a0
conao3 <conao3@gmail.com>
2019-05-17 00:34:20 +0900

    add persp-mode config

--- .dotfiles/.emacs.d/init.el
+++ .dotfiles/.emacs.d/init.el
@@ -500,6 +500,62 @@
                   (("<\\([[:alnum:]-]+\\)>") . ("\\1"))))
              (which-key-mode . t)))
 
+  (leaf persp-mode
+    :ensure t
+    :custom `((persp-keymap-prefix                 . ,(kbd "C-c p"))
+              (persp-nil-name                      . "default")
+              (persp-set-last-persp-for-new-frames . nil)
+
+              (persp-mode . t))
+    :hook ((emacs-startup-hook . toggle-frame-maximized))
+    :config
+    ;; NOTE: Redefine `persp-add-new' to raddress.
+    ;; Issue: Unable to create/handle persp-mode
+    ;; https://github.com/Bad-ptr/persp-mode.el/issues/96
+    ;; https://github.com/Bad-ptr/persp-mode-projectile-bridge.el/issues/4
+    ;; https://emacs-china.org/t/topic/6416/7
+    ;; (defun* persp-add-new (name &optional (phash *persp-hash*))
+    ;;   "Create a new perspective with the given `NAME'. Add it to `PHASH'.
+    ;; Return the created perspective."
+    ;;   (interactive "sA name for the new perspective: ")
+    ;;   (if (and name (not (equal "" name)))
+    ;;       (destructuring-bind (e . p)
+    ;;           (persp-by-name-and-exists name phash)
+    ;;         (if e p
+    ;;           (setq p (if (equal persp-nil-name name)
+    ;;       		nil (make-persp :name name)))
+    ;;           (persp-add p phash)
+    ;;           (run-hook-with-args 'persp-created-functions p phash)
+    ;;           p))
+    ;;     (message "[persp-mode] Error: Can't create a perspective with empty name.")
+    ;;     nil))
+
+    ;; Ignore temporary buffers
+    ;; (add-hook 'persp-common-buffer-filter-functions
+    ;;           (lambda (b) (or (string-prefix-p "*" (buffer-name b))
+    ;;       	       (string-prefix-p "magit" (buffer-name b)))))
+
+    (leaf *ivy-integration
+      :url "https://gist.github.com/Bad-ptr/1aca1ec54c3bdb2ee80996eb2b68ad2d#file-persp-ivy-el"
+      :config
+      (with-eval-after-load "ivy"
+        (add-hook 'ivy-ignore-buffers
+                  #'(lambda (b)
+                      (when persp-mode
+                        (let ((persp (get-current-persp)))
+                          (if persp
+                              (not (persp-contain-buffer-p b persp))
+                            nil)))))
+
+        (setq ivy-sort-functions-alist
+              (append ivy-sort-functions-alist
+                      '((persp-kill-buffer   . nil)
+                        (persp-remove-buffer . nil)
+                        (persp-add-buffer    . nil)
+                        (persp-switch        . nil)
+                        (persp-window-switch . nil)
+                        (persp-frame-switch  . nil)))))))
+
   (leaf avy
     :ensure t
     :bind (("M-o a a" . avy-goto-word-1)
